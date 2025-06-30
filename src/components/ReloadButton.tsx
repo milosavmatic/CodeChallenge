c57624d3e02f5cfb42ca240172d320de19f9b8f7
Milosav <milosavmatic@yahoo.com>
2025-06-30 23:25:50 +0200

    style: adjust positioning and hover effect in ReloadButton component

--- src/components/ReloadButton.tsx
+++ src/components/ReloadButton.tsx
@@ -6,8 +6,8 @@ interface ReloadButtonProps {
 
 export default function ReloadButton({ onReload }: ReloadButtonProps) {
   return (
-    <button className="w-7 h-7 md:w-16 md:h-16 top-5 right-5 absolute md:top-3 md:right-3" onClick={onReload}>
-      <ReloadIcon className="cursor-pointer transition-transform duration-300 hover:-rotate-90 text-white w-7 h-7 md:w-16 md:h-16 " />
+    <button className="w-7 h-7 md:w-16 md:h-16 top-5 right-3 absolute md:top-3" onClick={onReload}>
+      <ReloadIcon className="cursor-pointer transition-transform duration-300 md:hover:-rotate-90 text-white w-7 h-7 md:w-14 md:h-14" />
     </button>
   )
 }
