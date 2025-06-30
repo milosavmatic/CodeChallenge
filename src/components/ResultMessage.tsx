1f2755f50e91f8eb1fe1362bdf9324822c3bdbb9
Milosav <milosavmatic@yahoo.com>
2025-06-30 18:01:27 +0200

    refactor: improve ResultMessage component structure

--- src/components/ResultMessage.tsx
+++ src/components/ResultMessage.tsx
@@ -7,7 +7,7 @@ interface ResultMessageProps {
 
 export default function ResultMessage({ gameResult }: ResultMessageProps) {
   return (
-    <div className="text-4xl md:text-[72px] bg-white py-2 md:py-10 px-4 flex items-center justify-center w-10/12 md:min-w-[600px] whitespace-nowrap">
+    <div className="text-4xl md:text-[72px] bg-white py-2 md:py-10 px-4 items-center justify-center w-10/12 md:min-w-[600px] whitespace-nowrap hidden md:flex">
       {getResultMessage(gameResult)}
     </div>
   )
